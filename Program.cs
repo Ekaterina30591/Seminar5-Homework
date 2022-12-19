ba02771fb4c1ddb7f1c4ec2b8cdc80f2e6033407
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-19 22:50:07 +0300

    Task 3 done

--- Program.cs
+++ Program.cs
@@ -99,3 +99,65 @@ ShowArray(myArray);
 int result = OddIndexSum(myArray);
 Console.WriteLine($"Sum of array elements witn odd index is {result}");
 */
+
+// Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+/*
+double[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    double[] array = new double[size];
+        
+    for (int i = 0; i < size; i++)
+    {
+       array[i] = Math.Round(new Random().Next(minValue, maxValue) + new Random().NextDouble(), 2);
+        
+    }
+    return array;
+}
+
+void ShowArray(double[] array)
+
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
+
+double Diff (double[]array)
+{
+    int iMax = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[iMax] < array[i])
+        iMax = i;
+    }
+    int iMin = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[iMin] > array[i])
+        iMin = i;
+    }
+    double arrayMax = array[iMax];
+    double arrayMin = array[iMin];
+    double result = arrayMax - arrayMin;
+
+    return result;
+}
+
+Console.Write("Input number of elements: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input minimum value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input maximum value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[] myArray = CreateRandomArray(length, min, max);
+ShowArray(myArray);
+
+double result = Math.Round(Diff(myArray), 2);
+
+Console.WriteLine($"Different between max and min elements of the array is {result}");
+*/
