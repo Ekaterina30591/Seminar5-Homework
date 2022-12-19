56c865a2004db7410ab5d6531b0adf3d5d135156
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-19 22:46:00 +0300

    Task 2 done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+﻿/*
+//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 
 int[] CreateRandomArray(int size, int minValue, int maxValue)
@@ -47,4 +48,54 @@ int[] myArray = CreateRandomArray(length, min, max);
 ShowArray(myArray);
 int result = CountEven(myArray);
 Console.WriteLine($"Number of even numbers in array is {result}");
+*/
+/*
+//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
 
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
+
+int OddIndexSum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+        sum = sum + array[i];
+    }
+
+    return sum;
+}
+
+Console.Write("Input number of elements: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input minimum value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input maximum value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(length, min, max);
+ShowArray(myArray);
+int result = OddIndexSum(myArray);
+Console.WriteLine($"Sum of array elements witn odd index is {result}");
+*/
